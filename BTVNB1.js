c96e90952556bd22ebe7f351ab5e166ece7f0def
duykhanhit <hdkhanhit@gmail.com>
2020-11-16 13:19:42 +0700

    update btvn b1

--- BTVNB1.js
+++ BTVNB1.js
@@ -1,6 +1,6 @@
 /* 
   - Bài 1:
-    a) Viết hàm truyển vào 1 mảng person và giới tính trả về số lượng thành viên theo giới tính.
+    a) Viết hàm truyền vào 1 mảng person và giới tính trả về số lượng thành viên theo giới tính.
     b) Viết hàm truyền vào 1 mảng person, trả về danh sách thống kê dạng: 
     {
       treCon: 5,
@@ -10,7 +10,7 @@
     Điều kiện : - dưới 18 trẻ con
                 - từ 18 - 30 thanh niên
                 - sau 30 người già
-    Với data cho sẵn.
+    Với data cho sẵn. Sau mỗi phần đều in ra màn hình kết quả.
 */
 
 const listPersons = [
@@ -52,23 +52,25 @@ main();
     b) Viết hàm truyền vào 1 danh sách tài khoản, id tài khoản, số tiền để nạp tiền cho 1 tài khoản theo id đó.
         Kiểm tra nếu balance = 0 thì khuyến mại nạp lần đầu cho tài khoản đó 200%.
         Còn lại với các tài khoản khác sẽ không có khuyến mại.
+    c) Viết hàm kiểm tra tài khoản nào có thuộc tính isActive thì set thành true sau đó in ra màn hình.
+    ---Sau mỗi phần đều in ra màn hình kết quả.---
 */
 
 const listAccounts = [
   { id: 1, username: 'aduvip', balance: 1000000, type: 'normal' },
   { id: 2, username: 'aduvip1', balance: 500000, type: 'normal' },
   { id: 3, username: 'aduvip2', balance: 0, type: 'normal' },
-  { id: 4, username: 'aduvip3', balance: 20000, type: 'normal' },
+  { id: 4, username: 'aduvip3', balance: 20000, type: 'normal', isActive: false },
   { id: 5, username: 'aduvip4', balance: 2500000, type: 'normal' },
-  { id: 6, username: 'aduvip5', balance: 0, type: 'normal' },
+  { id: 6, username: 'aduvip5', balance: 0, type: 'normal', isActive: false },
   { id: 7, username: 'aduvip6', balance: 1300000, type: 'normal' },
   { id: 8, username: 'aduvip7', balance: 2200000, type: 'normal' },
-  { id: 9, username: 'aduvip8', balance: 1450000, type: 'normal' },
+  { id: 9, username: 'aduvip8', balance: 1450000, type: 'normal', isActive: false },
   { id: 10, username: 'aduvip9', balance: 0, type: 'normal' },
-  { id: 11, username: 'aduvip10', balance: 780000, type: 'normal' },
+  { id: 11, username: 'aduvip10', balance: 780000, type: 'normal', isActive: false },
   { id: 12, username: 'aduvip11', balance: 560000, type: 'normal' },
-  { id: 13, username: 'aduvip12', balance: 0, type: 'normal' },
+  { id: 13, username: 'aduvip12', balance: 0, type: 'normal', isActive: false },
   { id: 14, username: 'aduvip13', balance: 430000, type: 'normal' },
-  { id: 15, username: 'aduvip14', balance: 990000, type: 'normal' },
+  { id: 15, username: 'aduvip14', balance: 990000, type: 'normal', isActive: false },
   { id: 16, username: 'aduvip15', balance: 10000, type: 'normal' },
 ]
